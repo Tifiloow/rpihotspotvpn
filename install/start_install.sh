9982184b541b8977bb0169eff92f97b2328bc2da
Tifiloow <theophilejeromerocher44@gmail.com>
2019-11-18 20:35:18 +0100

    finished install and uninstall, have to test

--- install/start_install.sh
+++ install/start_install.sh
@@ -1,33 +1,34 @@
 sudo apt-get update -y
 sudo apt-get upgrade -y
-sudo apt install dnsmasq hostapd -y
+sudo apt-get install dnsmasq hostapd -y
 sudo systemctl stop dnsmasq
 sudo systemctl stop hostapd
-
-
+sudo apt-get install openssl
+sudo apt-get install npm
 sudo npm install body-parser mongoose bcryptjs express jsonwebtoken -y
-
+#install requierements
 
 echo 'static ip_address=192.168.4.1/24' >> /etc/dhcpcd.conf
 echo 'nohook wpa_supplicant' >> /etc/dhcpcd.conf
+#define static ip
 
 sudo service dhcpcd restart
 
-sudo openssl req -nodes -new -x509 -keyout server.key -out server.cert -subj "/C=FR/ST=./L=./O=./CN=."
+sudo openssl req -nodes -new -x509 -keyout server.key -out server.cert -subj "/C=FR/ST=./L=./O=./CN=." #create ssl files 
 
 
-sudo mv /etc/dnsmasq.conf /etc/dnsmasq.conf.orig
+sudo mv /etc/dnsmasq.conf /etc/dnsmasq.conf.orig #config dnsmasq
 sudo cp ./files/dnsmasq.conf /etc/ 
 
 
 sudo systemctl start dnsmasq
 sudo systemctl reload dnsmasq
 
-sudo cp ./files/hostapd.conf /etc/hostapd/hostapd.conf
+sudo cp ./files/hostapd.conf /etc/hostapd/hostapd.conf #set the new conf file of hostapd
 
 
 
-sudo sed -i -e "s/#DAEMON_CONF/DAEMON_CONF=\"\/etc\/hostapd\/hostapd.conf\"/g" /etc/default/hostapd
+sudo sed -i -e "s/#DAEMON_CONF/DAEMON_CONF=\"\/etc\/hostapd\/hostapd.conf\"/g" /etc/default/hostapd #uncommentif and defining conf file
 
 
 sudo systemctl unmask hostapd
@@ -35,14 +36,14 @@ sudo systemctl enable hostapd
 sudo systemctl start hostapd
 
 
-sudo sed -i -e "s/#net.ipv4.ip_forward=1/#net.ipv4.ip_forward=1/g" /etc/sysctl.conf
+sudo sed -i -e "s/#net.ipv4.ip_forward=1/#net.ipv4.ip_forward=1/g" /etc/sysctl.conf #setting ipv4 forward
 
 
 sudo iptables -t nat -A  POSTROUTING -o eth0 -j MASQUERADE
-sudo sh -c "iptables-save > /etc/iptables.ipv4.nat"
+sudo sh -c "iptables-save > /etc/iptables.ipv4.nat" #save iptable 
 
 
-sudo sed -i -e "s/exit 0//g" /etc/rc.local
+sudo sed -i -e "s/exit 0//g" /etc/rc.local #define the iptable restore
 echo 'iptables-restore < /etc/iptables.ipv4.nat' >> /etc/rc.local
 echo 'exit 0' >> /etc/rc.local
 #https://www.raspberrypi.org/documentation/configuration/wireless/access-point.md
